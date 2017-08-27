8e419b8454eddf877b111e34581fa85c6d29aaab
ylmbtm <ylmbtm@163.com>
2017-08-27 18:03:48 +0800

    提交服务器代码

--- Proto/gen_cs.bat
+++ Proto/gen_cs.bat
@@ -4,6 +4,6 @@ cd ..
 call gen_cs_real.bat
 del gen_cs_real.bat
 
-xcopy  .\protoc-gen-cs\*.* ..\Client\Assets\Scripts\Network\Proto\ /S /C /Y
+xcopy  .\protoc-gen-cs\*.* ..\Client\Assets\Scripts\Network\ /S /C /Y
 
 pause
