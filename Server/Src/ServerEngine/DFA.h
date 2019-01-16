75850fe64ffc3974fe0d0a9ce7bc6f58884660e7
M <ylmbtm@163.com>
2019-01-16 18:00:58 +0800

    Update DFA.h
    解决部分人因为编码问题，编译不过．

--- Server/Src/ServerEngine/DFA.h
+++ Server/Src/ServerEngine/DFA.h
@@ -13,44 +13,35 @@ public:
 
 	~DFANode();
 
-	//加入了个子节点
 	DFANode* AddNode(KeyType key);
 
 	BOOL AddNode(const KeyType* key, INT32 len);
 
-	//获取包含 key的子节点
 	DFANode* GetNode(KeyType key) const;
 
-	/**判断是否包含指定的key*/
 	BOOL HasKeyWord(const KeyType* pdata, INT32 len, BOOL bReturn = TRUE);
 
-	//加载关键字文件
 	BOOL LoadFile(const std::string& filename);
 
-	//判断是否含有关键字 是的话true
 	BOOL IsFileterWord(const std::string& word);
 
-	//把字符串中的所有敏感词都替换成"***"
 	BOOL FilterKeyWords(std::string& word, const std::string& dest = "*");
 
 private:
 	void GetKeyPos(INT32& nStart, INT32& nLen);
 
-	//清空节点数据
 	void ClearChildNode();
 
-	//判断子节点是否含有收尾服
 	BOOL HasEnding();
 
-	//获取子节点个数
 	INT32 GetChildCount() const;
 protected:
 	KeyType		m_key;
 	INT32		m_start;
-	DFANodeMap	m_childNodeMap;		//所有子节点
+	DFANodeMap	m_childNodeMap;
 
-	static INT32 s_length;		//记录匹配到的长度
-	static INT32 s_maxlen;		//记录最佳匹配长度
+	static INT32 s_length;
+	static INT32 s_maxlen;
 };
 
 #endif
