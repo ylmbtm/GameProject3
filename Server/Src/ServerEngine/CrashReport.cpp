d2e6744add023cb885790c286897766fbef469a7
qq587282 <587282@qq.com>
2019-04-30 12:26:08 +0800

    Update CrashReport.cpp
    增加linux 下的 Crash 生成core文件

--- Server/Src/ServerEngine/CrashReport.cpp
+++ Server/Src/ServerEngine/CrashReport.cpp
@@ -58,11 +58,46 @@ void UnSetCrashReport()
 
 
 #else
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+#include <string.h>
+#include <time.h>
+
+std::string g_AppName; 
+
 void  SetCrashReport(std::string strAppName)
 {
+	g_AppName = strAppName;
+	/*捕获异常信息 start*/
+	signal(SIGABRT, &exceptionalStack); //异常终止(abort)
+	signal(SIGBUS, &exceptionalStack); //硬件故障
+	signal(SIGFPE, &exceptionalStack); //算术异常
+	signal(SIGILL, &exceptionalStack); //非法硬件指令
+	signal(SIGIOT, &exceptionalStack); //硬件故障
+	signal(SIGQUIT, &exceptionalStack); //终端退出符
+	signal(SIGSEGV, &exceptionalStack); //无效存储访问
+	signal(SIGSYS, &exceptionalStack); //无效系统调用
+	signal(SIGTRAP, &exceptionalStack); //硬件故障
+	signal(SIGXCPU, &exceptionalStack); //超过CPU限制(setrlimit)
+	signal(SIGXFSZ, &exceptionalStack); //超过文件长度限制(setrlimit)
+	/*捕获异常信息 end*/
+
+	setvbuf(stdout, NULL, _IONBF, 0);
 	return ;
 }
 
+void exceptionalStack(int signal)
+{
+	time_t nowtime;
+	time(&nowtime);
+	pid_t pid = getpid();
+	char cmd[256];
+	sprintf(cmd, "gstack %d > core_%s_log_%d.core", pid, g_AppName.c_str(),nowtime);
+	system(cmd);
+	exit(-1);
+	
+}
 void UnSetCrashReport()
 {
 	return;
