e1b39bfd45cca155501f363f5339345cfe1ae576
Estone <iestone@yeah.net>
2020-06-01 20:23:18 +0800

    update linux start script

--- Server/Src/Linux/startserver.sh
+++ Server/Src/Linux/startserver.sh
@@ -1,11 +1,24 @@
-./AccountServer &
-./LoginServer &
-./DBServer &
-./LogicServer &
-./ProxyServer &
-./LogServer &
-./CenterServer &
-./GameServer id=1 port=9008 &
+nohup ./ProxyServer >> proxy.log 2>&1 &
+echo "$!" > proxy.pid
 
+nohup ./AccountServer >> account.log 2>&1 &
+echo "$!" > account.pid
 
+nohup ./LoginServer >> login.log 2>&1 &
+echo "$!" > login.pid
+
+nohup ./LogServer >> log_svr.log 2>&1 &
+echo "$!" > logsvr.pid
+
+nohup ./CenterServer >> center.log 2>&1 &
+echo "$!" > center.pid
+
+nohup ./DBServer >> db.log 2>&1 &
+echo "$!" > dbsvr.pid
+
+nohup ./LogicServer >> logic.log 2>&1 &
+echo "$!" > logic.pid
+
+nohup ./GameServer id=1 port=9008 >> game1.log 2>&1 &
+echo "$!" > game1.pid
  
